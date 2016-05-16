6e10537f6ea77bc90a4732032737de8d69982f21
Jason Bensel <benselj@empireco.com>
2016-05-16 13:49:21 -0400

    Broke apart GET line, added test files(Not working yet)

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,15 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
 public class WebServer {
-	
-	//localhost:8080/hi_there.html
+
+//	Steps in completing project:
+//	Complete the in-class assignment:
+//		Create a Server socket, accept a connection, set up the input and output.
+//		Read the "GET" line from the client.
+//		Use a loop to read all the request headers. Print them to the standard output.
+//	Parse the "GET" line (e.g., using String.split.)
+//	Open the requested file.
+//	Print the required response headers (Content-type and Content-length) (You may base the content-type on the file extension.)
+//	Use a loop to
+//		Read a block of data from the file then
+//		Write the data to the socket
+//	For a text file, you can read and write one line at a time. For a data file (like an image), you will want to use the raw read method to read a block of data (typically a few Kilobytes), then use the corresponding write method to write it to the socket. The read method typically returns the amount of data read, which may be less than your buffer size. You can write a text file this way also (which means you don't need separate code to handle text and data).
+//			Finally, close the socket.
+//  To test your server
+//  Create a folder named "data" in your project,
+//  Put some test files in this folder (e.g., a few .html files, a few .txt files and a few images),
+//	Launch your server on some port (e.g., 8080) from your project directory (i.e., the one that contains data)
+//	Point your web browser to "localhost:8080/data/file1.html"
 	
 	public static void handleConnection(Socket socket) throws IOException{
 		
@@ -19,29 +32,92 @@ public class WebServer {
 
 	    // Create a PrintStream than can write to the socket
 	    // Passing "true" as the second parameter causes each write to be followed by a flush.
-	    PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-	    
+	    PrintStream output = new PrintStream(socket.getOutputStream(), true);
+
+
+		/**
+		 * SERVER RESPONSE
+		 */
+		output.println("HTTP/1.1 200 OK");
+		output.println("Content-Type: text/plain");
+		output.println("Content-Length: 70");
+		output.println("Connection: close");
+		output.println("");
+
+
+		/**
+		 * Resource data of URL
+		 */
+		String[] resource = {};
+		String fileType = "";
+
+		/**
+		 * Reads the server response
+		 */
 	    String line;
 	    while((line = input.readLine())!=null){
 	    	if(line.length() == 0){
 	    		break;
 	    	}
+			/**
+			 * Parse the GET line to receive the correct file and file extension
+			 */
+			if(line.substring(0, 3).equals("GET")){
+				String getResource;
+				getResource = line.substring(4);
+				for(int i = 0; i < getResource.length(); i++){
+
+					//Separates the file from the remaining 'GET' string
+					if(getResource.charAt(i) == ' '){
+						getResource = getResource.substring(0, i);
+					}
+				}
+				resource = getResource.split("/");
+			}
+
+			/**
+			 * Find the file extension type for determining how to serve the
+			 * document
+			 */
+			for(String temp: resource){
+				for(int i = 0; i < temp.length(); i++){
+					if(temp.charAt(i)=='.'){
+						fileType = temp.substring(i+1);
+					}
+				}
+			}
+
+			/**
+			 * Prints first section of the server response
+			 * GET:
+			 * Host:
+			 * Connection:
+			 * etc..
+			 */
 	    	System.out.println(line);
+
+			/**
+			 * Serve the file. Display the correct file type using the file extension
+			 * NOTE: THIS WILL BE REPLACED WITH FILE CONTENT
+			 */
+			output.println(line);
+
 	    }
-	    
-	    output.println("HTTP/1.1 200 OK");
-	    output.println("Content-Type: text/plain");
-	    output.println("Content-Length: 70");
-	    output.println("Connection: close");
-	    output.println("");
-	    output.println("This is not the real content because this server is not yet complete.");
-	    
+
+		//(TEMP)CHECKS FOR CORRECT RESOURCE DATA AND FILE EXTENSION
+		for(String pos: resource){
+			System.out.println(pos);
+		}
+		System.out.print(fileType);
+
 	    output.flush();
 	    
 	    input.close();
 	    output.close();
 	    socket.close();
 	}
+
+
 	public static void main(String[] args) throws IOException {
 		final int DEFAULT_PORT = 8080; // For security reasons, only root can use ports < 1024.
         int portNumber = args.length > 1 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
